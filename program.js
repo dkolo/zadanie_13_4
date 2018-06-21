099e747b75c893ecc681c8b2118a16e0b790dfb2
Dominik Kolodziej <kolodziej.dominik@gmail.com>
2018-06-21 15:42:15 +0200

    Move getOSinfo function to file OSinfo.js

--- program.js
+++ program.js
@@ -1,4 +1,5 @@
-process.stdin.setEncoding('utf-8');
+var os = require('os');
+
 process.stdin.on('readable', function() {
     var input = process.stdin.read();
     if (input !== null) {
@@ -21,21 +22,3 @@ process.stdin.on('readable', function() {
     }
 });
 
-function getOSinfo() {
-    var type = os.type();
-    if (type === 'Darwin') {
-        type = 'OSX';
-    } else if (type === 'Windows_NT') {
-        type = 'Windows';
-    }
-    var release = os.release();
-    var cpu = os.cpus()[0].model;
-    var uptime = os.uptime();
-    var userInfo = os.userInfo();
-    console.log('System:', type);
-    console.log('Release:', release);
-    console.log('CPU model:', cpu);
-    console.log('Uptime: ~', (uptime / 60).toFixed(0), 'min');
-    console.log('User name:', userInfo.username);
-    console.log('Home dir:', userInfo.homedir);
-}
