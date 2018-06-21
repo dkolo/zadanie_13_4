ac1235435d30a90d08754de22efb189890f3e944
Dominik Kolodziej <kolodziej.dominik@gmail.com>
2018-06-21 18:41:20 +0200

    Fix bug with timeFormat module export name

--- modules/OSinfo.js
+++ modules/OSinfo.js
@@ -1,5 +1,5 @@
 var os = require('os');
-var time = require('./timeFormat')
+var timeFormat = require('./timeFormat');
 
 function getOSinfo() {
     var type = os.type();
@@ -14,7 +14,7 @@ function getOSinfo() {
     console.log('System:', type);
     console.log('Release:', release);
     console.log('CPU model:', cpu);
-    timeFormat.time();
+    timeFormat.clock();
     console.log('User name:', userInfo.username);
     console.log('Home dir:', userInfo.homedir);
 }
--- modules/timeFormat.js
+++ modules/timeFormat.js
@@ -1,11 +1,12 @@
 var os = require('os');
 
-function timeFormat() {
+function time() {
     var uptime = os.uptime();
     var hours = (uptime / 3600).toFixed(0);
     var minutes = (uptime / 60).toFixed(0);
     var seconds = (uptime % 60);
-    var console.log('Uptime: ~' + hours + ' godz. ' + minutes + ' min. ' + seconds + ' sek.');
+
+    console.log('Uptime: ~' + hours + ' godz. ' + minutes + ' min. ' + seconds + ' sek.');
 }
 
-exports.time = timeFormat;
+exports.clock = time;
