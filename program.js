ef7597f08b8561d8bd4c57e38c478c4bf5e83eec
Dominik Kolodziej <kolodziej.dominik@gmail.com>
2018-06-21 15:28:04 +0200

    Fork program.js file from a different project and add getOSinfo function

---
+++ program.js
@@ -0,0 +1,41 @@
+process.stdin.setEncoding('utf-8');
+process.stdin.on('readable', function() {
+    var input = process.stdin.read();
+    if (input !== null) {
+        var instruction = input.toString().trim();
+        switch (instruction) {
+            case '/exit':
+                process.stdout.write('Quitting app!\n');
+                process.exit();
+                break;
+            case '/info':
+                process.stdout.write('Twoja wersja Node.js: ' + process.versions.node + '\n' + 'Język ustawiony w systemie : ' + process.env.LANG);
+                process.exit();
+                break;
+            case '/getOSinfo':
+                getOSinfo();
+                break;
+            default:
+                process.stderr.write('Wrong instruction!');
+        }
+    }
+});
+
+function getOSinfo() {
+    var type = os.type();
+    if (type === 'Darwin') {
+        type = 'OSX';
+    } else if (type === 'Windows_NT') {
+        type = 'Windows';
+    }
+    var release = os.release();
+    var cpu = os.cpus()[0].model;
+    var uptime = os.uptime();
+    var userInfo = os.userInfo();
+    console.log('System:', type);
+    console.log('Release:', release);
+    console.log('CPU model:', cpu);
+    console.log('Uptime: ~', (uptime / 60).toFixed(0), 'min');
+    console.log('User name:', userInfo.username);
+    console.log('Home dir:', userInfo.homedir);
+}
